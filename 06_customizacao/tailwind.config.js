5665f1b97245990ae55a9d7bfc89e30cbace655c
J-Breno <joao.breno85@hotmail.com>
2024-11-13 16:26:45 -0300

    customização

---
+++ 06_customizacao/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      black: '#010101',
+    },
+    extend: {
+      colors: {
+        'new-azul': '#4532ff'
+      }
+    },
+  },
+  plugins: [],
+}
+
